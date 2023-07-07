a100d5dc02f123d417ae7842875c0c7ea9e92cef
masendhy <massendhy@gmail.com>
2023-07-07 16:01:49 +0700

    used_car price_prediction almost done

---
+++ app.py
@@ -0,0 +1,20 @@
+import pickle
+import streamlit as st
+
+
+model = pickle.load(open('car_price_model.sav', 'rb'))
+
+st.title('Car Price Prediction')
+
+year = st.number_input('Input car product year')
+mileage = st.number_input('Input car mileage')
+tax = st.number_input('Input car tax')
+mpg = st.number_input('Input car mpg')
+engine_size = st.number_input('Input car engine size')
+
+prediction = ' '
+
+if st.button('Predict'):
+    prediction = model.predict([[year, mileage, tax, mpg, engine_size]])
+    st.write('The price of the car is EUR ',prediction)
+    st.write('The price of the car is IDR ',prediction*16400)
